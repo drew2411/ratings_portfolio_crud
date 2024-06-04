d29417f7881427e7266ced88a311ce995b862c08
Siddharth Malik <122918573+siddharth-malik456@users.noreply.github.com>
2024-06-04 17:44:17 +0400

    Finished integrating trpc

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,15 +7,20 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { TrpcRouter } from './trpc/trpc.router';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
+  app.enableCors();
+
+  // ADDING TRPC
+  const trpc = app.get(TrpcRouter);
+  trpc.applyMiddleware(app);
+  
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}`
   );
 }
 
