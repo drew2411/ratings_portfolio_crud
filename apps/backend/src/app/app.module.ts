4e9ff737299016296aaea79bc5b4b036e17e941d
Siddharth Malik <122918573+siddharth-malik456@users.noreply.github.com>
2024-06-04 20:17:44 +0400

    Added .env configuration and prisma

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrpcModule } from '@server/trpc/trpc.module';
+import { PrismaModule } from '@server/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TrpcModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TrpcModule, 
+    PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
