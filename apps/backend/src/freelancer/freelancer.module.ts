a17f62cd2f9196234fb780bb65d7412e94ed1efd
Joel Ajay <ajaijoel@gmail.com>
2024-06-17 12:04:18 +0530

    fix: removed controller from module

--- apps/backend/src/freelancer/freelancer.module.ts
+++ apps/backend/src/freelancer/freelancer.module.ts
@@ -2,15 +2,14 @@ import { Module } from '@nestjs/common';
 import { TrpcModule } from '@server/trpc/trpc.module';
 import { PrismaModule } from '@server/prisma/prisma.module';
 import { FreelancerService } from './freelancer.service';
-import { FreelancerController } from './freelancer.controller';
+import { FreelancerRouter } from './freelancer.router';
 
 @Module({
   imports: [
     AppModule,
     TrpcModule, 
     PrismaModule],
-  controllers: [FreelancerController],
   providers: [FreelancerService],
-  exports: [FreelancerRoute]
+  exports: [FreelancerRouter]
 })
 export class AppModule {}
